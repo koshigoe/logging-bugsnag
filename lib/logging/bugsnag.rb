8467446a5e66b523d1762fceb71374696abba9f2
koshigoe <koshigoeb@gmail.com>
2015-04-18 19:16:32 +0900

    initial commit.

---
+++ lib/logging/bugsnag.rb
@@ -0,0 +1,7 @@
+require "logging/bugsnag/version"
+
+module Logging
+  module Bugsnag
+    # Your code goes here...
+  end
+end
---
+++ lib/logging/bugsnag/version.rb
@@ -0,0 +1,5 @@
+module Logging
+  module Bugsnag
+    VERSION = "0.1.0"
+  end
+end
